61bd16f3e323ff7846885471551be1a29dbf3923
Mrab Ezreb <zerbebram@gmail.com>
2014-09-28 12:35:29 -0400

    I NEEDS HELP!
    I am updating this to contain an issue that I have with texture and
    language stuff.

--- src/main/java/com/ezreb/Infinite_Diamond.java
+++ src/main/java/com/ezreb/Infinite_Diamond.java
@@ -1,5 +1,6 @@
 package com.ezreb;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -13,8 +14,10 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class Infinite_Diamond
 {
     public static final String MODID = "Infinite Diamonds";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.0.1";
     public static final String NAME = "Infinite Diamonds";
+    
+    public static Item item1;
     
     @Instance(value = MODID)
     public static Infinite_Diamond instance;
@@ -24,11 +27,17 @@ public class Infinite_Diamond
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent preInitEvent) {
-    	Item item1 = new SuperDiamond();
+    	item1 = new SuperDiamond()
+    		.setMaxStackSize(1)
+			.setCreativeTab(CreativeTabs.tabMaterials)
+			.setUnlocalizedName("superDiamond")
+			.setTextureName("infiniteDiamonds:superDiamond");
+    		//.func_111206_d("infiniteDiamonds:superDiamond");
     	GameRegistry.registerItem(item1, "superDiamondEzreb");
+    	
     }
-    
-    @EventHandler
+
+	@EventHandler
     public void init(FMLInitializationEvent event)
     {
         System.out.println("You want diamonds? Fine, dupe them. Just be sure to have somthing to do!");
