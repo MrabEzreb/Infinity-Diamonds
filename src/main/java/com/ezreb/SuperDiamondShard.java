443bd040a0a875dc5f2d8bbfe605b844bd3da655
MrabEzreb <zerbebram@gmail.com>
2014-09-30 10:57:18 -0400

    From school to home.
    Added in registerers for the many pieces of a super diamond.

--- src/main/java/com/ezreb/SuperDiamondShard.java
+++ src/main/java/com/ezreb/SuperDiamondShard.java
@@ -6,11 +6,39 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class SuperDiamondShard extends Item {
-	SuperDiamondShard() {
-		this.setMaxStackSize(1)
+	SuperDiamondShard(int size) {
+		String name;
+		if (size==1) {
+			name = "part";
+		}
+		if (size==2) {
+			name = "shard";
+		}
+		if (size==3) {
+			name = "chip";
+		}
+		if (size==4) {
+			name = "refinedchip";
+		}
+		if (size==5) {
+			name = "cooledchip";
+		}
+		if (size==6) {
+			name = "refinedshard";
+		}
+		if (size==7) {
+			name = "diamondfacet";
+		}
+		if (size==8) {
+			name = "diamondpart";
+		}
+		if (size==9) {
+			name = "rawdiamond";
+		}
+		this.setMaxStackSize(64)
 		.setCreativeTab(CreativeTabs.tabMaterials)
-		.setUnlocalizedName("superDiamondShard")
-		.setTextureName("infinitediamonds"+":superdiamondshard");
+		.setUnlocalizedName("superDiamond"+name)
+		.setTextureName("infinitediamonds"+":superdiamond"+name);
 //		registerIcons(func_111206_d());
 		//func_111206_d("infiniteDiamonds:superDiamond");
 		//setMaxStackSize(1);
