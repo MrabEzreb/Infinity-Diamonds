b09b63bc0305f0a94fb7d7fb5da6396f25b903c1
Mrab Ezreb <zerbebram@gmail.com>
2014-09-30 15:33:38 -0400

    Revert "From school to home."
    This reverts commit 443bd040a0a875dc5f2d8bbfe605b844bd3da655.

--- src/main/java/com/ezreb/SuperDiamondShard.java
+++ src/main/java/com/ezreb/SuperDiamondShard.java
@@ -6,39 +6,11 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class SuperDiamondShard extends Item {
-	SuperDiamondShard(int size) {
-		String name;
-		if (size==1) {
-			name = "part";
-		}
-		if (size==2) {
-			name = "shard";
-		}
-		if (size==3) {
-			name = "chip";
-		}
-		if (size==4) {
-			name = "refinedchip";
-		}
-		if (size==5) {
-			name = "cooledchip";
-		}
-		if (size==6) {
-			name = "refinedshard";
-		}
-		if (size==7) {
-			name = "diamondfacet";
-		}
-		if (size==8) {
-			name = "diamondpart";
-		}
-		if (size==9) {
-			name = "rawdiamond";
-		}
-		this.setMaxStackSize(64)
+	SuperDiamondShard() {
+		this.setMaxStackSize(1)
 		.setCreativeTab(CreativeTabs.tabMaterials)
-		.setUnlocalizedName("superDiamond"+name)
-		.setTextureName("infinitediamonds"+":superdiamond"+name);
+		.setUnlocalizedName("superDiamondShard")
+		.setTextureName("infinitediamonds"+":superdiamondshard");
 //		registerIcons(func_111206_d());
 		//func_111206_d("infiniteDiamonds:superDiamond");
 		//setMaxStackSize(1);
